0b3db6e26cd75b9ea16bd527f07568ab7923fa1c
G-Nanda <nandamokesh927@gmail.com>
2024-01-25 23:40:34 +0530

    updated

---
+++ script.js
@@ -0,0 +1,8 @@
+var typed = new Typed(".multiple-text",{
+    strings:["Web Developer","IoT Explorer","Quick Learner"],
+    typeSpeed:100,
+    backSpeed:100,
+    backDelay:1000,
+    loop:true
+
+})
